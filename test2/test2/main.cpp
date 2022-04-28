1fa9ff3f297b3777fcdaf8ec60e00a28e850d5bb
Sevinch Rustamova <ac2029413@akfauniversity.org>
2022-04-28 15:21:24 +0500

    Working with two computers
    Hi, there

---
+++ test2/test2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by SEVINCH RUSTAMOVA on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, Sevinch";
+    return 0;
+}
